7233592bde701a494ed8c549921cca78590f5b41
Jay Marcyes <jay@marcyes.com>
2013-06-27 20:11:47 -0700

    adds basic versioning in the VersionCall, this will allow us to seamlessly version our endpoints using the accept headers

--- endpoints.py
+++ endpoints.py
@@ -325,12 +325,177 @@ class Call(object):
             body = callback(*callback_args, **callback_kwargs)
             self.response.body = body
 
-        except CallError:
-            raise
+        except CallError, e:
+            self.response.code = e.code
+            self.response.body = e.message
 
         except Exception, e:
             self.response.code = 500
             self.body = e.message
 
         return self.response
+
+class VersionCall(Call):
+    """
+    versioning is based off of this post: http://urthen.github.io/2013/05/09/ways-to-version-your-api/
+    """
+
+    default_version = None
+    """set this to the default version if you want a fallback version, if this is None then version check is enforced"""
+
+    version_media_type = u""
+    """the media type that should be versioned, usually something like application/json"""
+
+    @property
+    def controller_prefix(self):
+        cp = u""
+        if hasattr(self, "_controller_prefix"):
+            cp = self._controller_prefix
+        v = self.get_version()
+        if cp:
+            cp += u".{}".format(v)
+        else:
+            cp = v
+
+        return cp
+
+    @controller_prefix.setter
+    def controller_prefix(self, v):
+        self._controller_prefix = v
+
+    def get_version(self):
+        if not self.version_media_type:
+            raise ValueError("You are versioning a call with no version_media_type")
+
+        v = None
+        h = self.request.headers
+        accept_header = h.get('accept', u"")
+        if not accept_header:
+            raise CallError(406, "Expected accept header with {} media type".format(self.version_media_type))
+
+        a = AcceptHeader(accept_header)
+        for mt in a.filter(self.version_media_type):
+            v = mt[2].get(u"version", None)
+            if v: break
+
+        if not v:
+            v = self.default_version
+            if not v:
+                raise CallError(406, "Expected accept header with {};version=N media type".format(self.version_media_type))
+
+        return v
+
+class AcceptHeader(object):
+    """
+    wraps the Accept header to allow easier versioning
+
+    provides methods to return the accept media types in the correct order
+    """
+    def __init__(self, header):
+        self.header = header
+        self.media_types = []
+
+        if header:
+            accepts = header.split(u',')
+            for accept in accepts:
+                accept = accept.strip()
+                a = accept.split(u';')
+
+                # first item is the media type:
+                media_type = self._split_media_type(a[0])
+
+                # all other items should be in key=val so let's add them to a dict:
+                params = {}
+                q = 1.0 # default according to spec
+                for p in a[1:]:
+                    pk, pv = p.strip().split(u'=')
+                    if pk == u'q':
+                        q = float(pv)
+                    else:
+                        params[pk] = pv
+
+                #pout.v(media_type, q, params)
+                self.media_types.append((media_type, q, params, accept))
+
+    def _split_media_type(self, media_type):
+        """return type, subtype from media type: type/subtype"""
+        media_type_bits = media_type.split(u'/')
+        return media_type_bits
+
+    def _sort(self, a, b):
+        '''
+        sort the headers according to rfc 2616 so when __iter__ is called, the accept media types are
+        in order from most preferred to least preferred
+        '''
+        ret = 0
+
+        # first we check q, higher values win:
+        if a[1] != b[1]:
+            ret = cmp(a[1], b[1])
+        else:
+            found = False
+            for i in xrange(2):
+                ai = a[0][i]
+                bi = b[0][i]
+                if ai == u'*':
+                    if bi != u'*':
+                        ret = -1
+                        found = True
+                        break
+                    else:
+                        # both *, more verbose params win
+                        ret = cmp(len(a[2]), len(b[2]))
+                        found = True
+                        break
+                elif bi == u'*':
+                    ret = 1
+                    found = True
+                    break
+
+            if not found:
+                ret = cmp(len(a[2]), len(b[2]))
+
+        return ret
+
+    def __iter__(self):
+        sorted_media_types = sorted(self.media_types, self._sort, reverse=True)
+        for x in sorted_media_types:
+            yield x
+
+    def filter(self, media_type, **params):
+        mtype, msubtype = self._split_media_type(media_type)
+        for x in self.__iter__():
+
+            # all the params have to match to make the media type valid
+            matched = True
+            for k, v in params.iteritems():
+                if x[2].get(k, None) != v:
+                    matched = False
+                    break
+
+            if matched:
+                if mtype == u'*':
+                    if msubtype == u'*':
+                        yield x
+                    else:
+                        if x[0][1] == msubtype:
+                            yield x
+                elif x[0][0] == mtype:
+                    if msubtype == u'*':
+                        yield x
+                    elif x[0][1] == msubtype:
+                        yield x
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- endpoints_test.py
+++ endpoints_test.py
@@ -35,47 +35,47 @@ class RequestTest(TestCase):
         self.assertEqual(urlparse.parse_qs(r.query, True), urlparse.parse_qs(query, True))
         self.assertEqual(r.query_kwargs, query_kwargs)
 
-    def test_instantiation(self):
-        return
-
-        r = endpoints.Request()
-
-        # we need to mimic the request object that comes from the mongrel2 python module
-        class Mongrel2MockRequest(object): pass
-        r = Mongrel2MockRequest()
-        r.sender = 'server-uuid'
-        r.ident = '4'
-        r.path = '/foo/bar'
-        r.headers = {
-            u"accept-language": u"en-US,en;q=0.5",
-            u"accept-encoding": u"gzip, deflate",
-            u"PATTERN": u"/",
-            u"x-forwarded-for": u"10.0.2.2",
-            u"host": u"localhost:4000",
-            u"accept": u"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            u"user-agent": u"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0",
-            u"connection": u"keep-alive",
-            u"VERSION": u"HTTP/1.1",
-            u"QUERY": u"foo=bar&che=baz",
-            u"PATH": u"/foo/bar",
-            u"METHOD": u"GET",
-            u"URI": u"/foo/bar?foo=bar&che=baz"
-        }
-        r.body = ''
-
-        rm = endpoints.Mongrel2Request(r)
-
-        self.assertEqual(rm.path, r.headers[u'PATH'])
-        self.assertEqual(rm.path_args, [u'foo', u'bar'])
-
-        self.assertEqual(rm.query, r.headers[u'QUERY'])
-        self.assertEqual(rm.query_kwargs, {u'foo': u'bar', u'che': u'baz'})
-
-        # test array query strings
-        rm_query = u"foo=bar&che=baz&foo=che"
-        rm.query = rm_query
-        self.assertEqual(rm.query, rm_query)
-        self.assertEqual(rm.query_kwargs, {u'foo': [u'bar', u'che'], u'che': u'baz'})
+#    def test_instantiation(self):
+#        return
+#
+#        r = endpoints.Request()
+#
+#        # we need to mimic the request object that comes from the mongrel2 python module
+#        class Mongrel2MockRequest(object): pass
+#        r = Mongrel2MockRequest()
+#        r.sender = 'server-uuid'
+#        r.ident = '4'
+#        r.path = '/foo/bar'
+#        r.headers = {
+#            u"accept-language": u"en-US,en;q=0.5",
+#            u"accept-encoding": u"gzip, deflate",
+#            u"PATTERN": u"/",
+#            u"x-forwarded-for": u"10.0.2.2",
+#            u"host": u"localhost:4000",
+#            u"accept": u"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#            u"user-agent": u"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0",
+#            u"connection": u"keep-alive",
+#            u"VERSION": u"HTTP/1.1",
+#            u"QUERY": u"foo=bar&che=baz",
+#            u"PATH": u"/foo/bar",
+#            u"METHOD": u"GET",
+#            u"URI": u"/foo/bar?foo=bar&che=baz"
+#        }
+#        r.body = ''
+#
+#        rm = endpoints.Mongrel2Request(r)
+#
+#        self.assertEqual(rm.path, r.headers[u'PATH'])
+#        self.assertEqual(rm.path_args, [u'foo', u'bar'])
+#
+#        self.assertEqual(rm.query, r.headers[u'QUERY'])
+#        self.assertEqual(rm.query_kwargs, {u'foo': u'bar', u'che': u'baz'})
+#
+#        # test array query strings
+#        rm_query = u"foo=bar&che=baz&foo=che"
+#        rm.query = rm_query
+#        self.assertEqual(rm.query, rm_query)
+#        self.assertEqual(rm.query_kwargs, {u'foo': [u'bar', u'che'], u'che': u'baz'})
 
 class CallTest(TestCase):
     def test_controller_info(self):
@@ -142,10 +142,111 @@ class CallTest(TestCase):
         # if it succeeds, then it passed the test :)
         d = c.get_callback_info()
 
+class VersionCallTest(TestCase):
+
+    def test_get_version(self):
+        r = endpoints.Request()
+        r.headers = {u'accept': u'application/json;version=v1'}
+
+        c = endpoints.VersionCall()
+        c.version_media_type = u'application/json'
+        c.request = r
+
+        v = c.get_version()
+        self.assertEqual(u'v1', v)
+
+        c.request.headers = {u'accept': u'application/json'}
+
+        with self.assertRaises(endpoints.CallError):
+            v = c.get_version()
+
+        c.default_version = u'v1'
+        v = c.get_version()
+        self.assertEqual(u'v1', v)
+
+    def test_controller_prefix(self):
+        r = endpoints.Request()
+        r.headers = {u'accept': u'application/json;version=v1'}
+
+        c = endpoints.VersionCall()
+        c.version_media_type = u'application/json'
+        c.request = r
+
+        cp = c.controller_prefix
+        self.assertEqual(u"v1", cp)
+
+        c.controller_prefix = "foo.bar"
+        cp = c.controller_prefix
+        self.assertEqual(u"foo.bar.v1", cp)
 
 
+class AcceptHeaderTest(TestCase):
 
+    def test_init(self):
+        ts = [
+            (
+                u"text/*, text/html, text/html;level=1, */*",
+                [
+                    u"text/html;level=1",
+                    u"text/html",
+                    u"text/*",
+                    u"*/*"
+                ]
+            ),
+            (
+                u'text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5',
+                [
+                    u"text/html;level=1",
+                    u"text/html;q=0.7",
+                    u"*/*;q=0.5",
+                    u"text/html;level=2;q=0.4",
+                    "text/*;q=0.3",
+                ]
+            ),
+        ]
 
+        for t in ts:
+            a = endpoints.AcceptHeader(t[0])
+            for i, x in enumerate(a):
+                self.assertEqual(x[3], t[1][i])
+
+    def test_filter(self):
+        ts = [
+            (
+                u"application/json",
+                (u"application/json", {}),
+                1
+            ),
+            (
+                u"application/json",
+                (u"application/*", {}),
+                1
+            ),
+            (
+                u"application/json",
+                (u"text/html", {}),
+                0
+            ),
+            (
+                u"application/json;version=v1",
+                (u"application/json", {u"version": u"v1"}),
+                1
+            ),
+            (
+                u"application/json;version=v2",
+                (u"application/json", {u"version": u"v1"}),
+                0
+            ),
+
+        ]
+
+        for t in ts:
+            a = endpoints.AcceptHeader(t[0])
+            count = 0
+            for x in a.filter(t[1][0], **t[1][1]):
+                count += 1
+
+            self.assertEqual(t[2], count)
 
 
 
