8607c0ea6ea4724d23923cf6013d7b679d4712c1
Jay Marcyes <jay@marcyes.com>
2016-03-01 14:08:46 -0800

    a little simpler setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,14 @@
 # http://docs.python.org/distutils/setupscript.html
 # http://docs.python.org/2/distutils/examples.html
 
-import sys
 from setuptools import setup, find_packages
-import ast
+import re
 import os
 
 
-name = 'endpoints'
-version = ''
+name = "endpoints"
 with open(os.path.join(name, "__init__.py"), 'rU') as f:
-    for node in (n for n in ast.parse(f.read()).body if isinstance(n, ast.Assign)):
-        node_name = node.targets[0]
-        if isinstance(node_name, ast.Name) and node_name.id.startswith('__version__'):
-            version = node.value.s
-            break
-
-if not version:
-    raise RuntimeError('Unable to find version number')
+    version = re.search("^__version__\s*=\s*[\'\"]([^\'\"]+)", f.read(), flags=re.I | re.M).group(1)
 
 
 setup(
