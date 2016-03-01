89d42565fe6efa9a7b553e662ecd1661b7041087
Jay Marcyes <jay@marcyes.com>
2016-03-01 11:41:25 -0800

    fixes setup.py problem where you couldn't import endpoints in setup.py because it needed modules setup.py installs, not sure why I didn't catch this but I'm embarassed

--- setup.py
+++ setup.py
@@ -5,10 +5,21 @@
 import sys
 from setuptools import setup, find_packages
 import ast
+import os
 
-import endpoints
-name = endpoints.__name__
-version = endpoints.__version__
+
+name = 'endpoints'
+version = ''
+with open(os.path.join(name, "__init__.py"), 'rU') as f:
+    for node in (n for n in ast.parse(f.read()).body if isinstance(n, ast.Assign)):
+        node_name = node.targets[0]
+        if isinstance(node_name, ast.Name) and node_name.id.startswith('__version__'):
+            version = node.value.s
+            break
+
+if not version:
+    raise RuntimeError('Unable to find version number')
+
 
 setup(
     name=name,
